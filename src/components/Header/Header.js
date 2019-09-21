59d2cd4b43904966409c9a798eb7c503e5ab8b26
Jonathan Smithies <jonathan@elev8now.co.uk>
2019-09-21 16:19:05 +0100

    added menu to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,12 @@ import React from "react";
 
 const Header = () => (
     <div className="page-header" >
-        <h1 className="m-2">Ping Pong <small>Tournament Team Generator</small></h1>
+        <h2 className="m-2 text-center">Ping Pong Tournament Team Generator</h2>
+        <ul className="nav justify-content-center">
+            <li className="nav-item m-2">Settings</li>
+            <li className="nav-item m-2">Tournament</li>
+            <li className="nav-item m-2">Results</li>
+        </ul>
     </div>
 )
 

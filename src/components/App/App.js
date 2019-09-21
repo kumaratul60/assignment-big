cd3292fbe4c306e43d394c4327f848b4471da7f4
Jonathan Smithies <jonathan@elev8now.co.uk>
2019-09-21 16:06:29 +0100

    added temporary draft header component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from "react";
+import Header from "../Header/Header"
 
 function App() {
     return (
         <>
-            <h1>Hello, world!</h1>
+            <Header />
         </>
     );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => (
+    <div className="page-header" >
+        <h1 className="m-2">Ping Pong <small>Tournament Team Generator</small></h1>
+    </div>
+)
+
+export default Header;
